50364987be5d4388591596294c1db739095c4271
nicolas <nicolas.grislain@gmail.com>
2015-02-01 03:09:22 +0100

    working

--- societe_generale.py
+++ societe_generale.py
@@ -1,38 +1,74 @@
-import urllib
-import webbrowser
+import os
+import os.path
 import time
-import thread
-from datetime import date, timedelta
+from datetime import date, timedelta, datetime
+import codecs
+import csv
+import json
+import locale
 # Import selenium
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
+# Import mysql connector
+import MySQLdb
 
-def login():
-    url = 'https://particuliers.societegenerale.fr/'
-    login_wait_time = 1
-    # Test the login
-    def test_login():
-        url = 'https://particuliers.secure.societegenerale.fr/restitution/tel_telechargement.html'
-        page = urllib.urlopen(url).read()
-        if 'Votre session a expir&eacute;. Merci de bien vouloir vous identifier &agrave; nouveau.' in page:
-            print 'Not logged in'
-            return False
-        else:
-            print 'OK'
-            return True
-    # Login
-    webbrowser.open(url)
-    # Wait for login
-    while not test_login():
-        time.sleep(login_wait_time)
-    
+SG_URL = 'https://particuliers.societegenerale.fr/'
+SG_CLIENT_ID = 30144911
+DOWNLOAD_PATH = '/Users/nicolas/Downloads'
+DB_HOST = 'grislain.fr'
+DB_USER = 'bankscraper'
+DB_PASSWD = 'YmFua3NjcmFwZXI='
+DB_NAME = 'bankscraper'
+# Set the locale of the bqnk
+locale.setlocale(locale.LC_NUMERIC, 'fr_FR.UTF-8')
 
-def get_transactions(client_id=30144911):
-    # Shows the login invite
-    login_url = 'https://particuliers.societegenerale.fr/'
+def get_last_download(download_path=DOWNLOAD_PATH):
+    results = []
+    for file in os.listdir(download_path):
+        if file.endswith(".tsv"):
+            results.append((os.path.join(download_path, file), time.ctime(os.path.getctime(os.path.join(download_path, file)))))
+    if len(results)>0:
+        return max(results, key=lambda k:k[1])[0]
+    else:
+        return None
+
+def utf_8_encoder(unicode_csv_data):
+    for line in unicode_csv_data:
+        yield line.encode('utf_8')
+
+def create_current_account_table():
+    db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD, db=DB_NAME)
+    cur = db.cursor()
+    cur.execute('''CREATE TABLE IF NOT EXISTS `current_account` (
+    `id` int(11) NOT NULL AUTO_INCREMENT,
+    `date` date DEFAULT NULL,
+    `name` char(128) NOT NULL,
+    `description` text,
+    `amount` decimal(12,2) NOT NULL,
+    `currency` char(3) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX (`date`,`name`,`amount`,`currency`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;''')
+    cur.close()
+    db.commit()
+    db.close()
+
+def push_current_account_data(data):
+    db = MySQLdb.connect(host=DB_HOST, user=DB_USER, passwd=DB_PASSWD, db=DB_NAME)
+    cur = db.cursor()
+    cur.executemany('''INSERT IGNORE INTO `current_account` (`date`, `name`, `description`, `amount`, `currency`)
+    VALUES (%s, %s, %s, %s, %s)
+    ''', [(datetime.strptime(row[0], '%d/%m/%Y').date(), row[1], row[2], locale.atof(row[3]), row[4]) for row in data])
+    cur.close()
+    db.commit()
+    db.close()
+
+def get_transactions(sg_client_id=SG_CLIENT_ID):
+    last_download = get_last_download()
+    # Init a Firefox browser
     profile = webdriver.FirefoxProfile()
     profile.set_preference('browser.download.panel.shown', True)
     profile.set_preference('browser.download.manager.showAlertOnStart', False)
@@ -40,8 +76,10 @@ def get_transactions(client_id=30144911):
     profile.set_preference('browser.download.manager.showWhenStarting', False)
     profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'application/x-ecriture-txt')
     driver = webdriver.Firefox(profile)
-    driver.get(login_url)
-    codcli = str(client_id)
+    # Shows the login invite
+    sg_url = SG_URL
+    driver.get(sg_url)
+    codcli = str(sg_client_id)
     driver.find_element_by_id('codcli').send_keys(codcli)
     driver.find_element_by_id('button').click()
     # Wait for the login process to complete
@@ -56,7 +94,24 @@ def get_transactions(client_id=30144911):
     driver.find_element_by_name('datedu').send_keys(datedu)
     # Get the data
     driver.find_element_by_css_selector('a[href="javascript:telecharger(this)"]').click()
+    while get_last_download() == last_download:
+        time.sleep(1)
+    time.sleep(3)
+    data = []
+    with codecs.open(get_last_download(), 'r', 'latin_1') as csv_file:
+        csv_reader = csv.reader(utf_8_encoder(csv_file), delimiter='\t', quotechar='"')
+        csv_reader.next()
+        csv_reader.next()
+        names = csv_reader.next()
+        for row in csv_reader:
+            data.append(row)
+    # Push the data to mysql
+    create_current_account_table()
+    push_current_account_data(data)
+    return names, data
 
-    
-get_transactions()
-
+names, data = get_transactions()
+with open('names.json','w') as names_file:
+    json.dump(names, names_file)
+with open('data.json','w') as data_file:
+    json.dump(data, data_file)
